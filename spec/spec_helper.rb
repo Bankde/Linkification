64b0537e64461178b78e954314cf45c24d39e33a
Bankde <bankde@hotmail.com>
2016-07-11 18:04:51 +0700

    add rule http:example.com - fail

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,10 +3,15 @@ require_relative '../makeLink.rb'
 
 
 describe "Test Linkification" do
-  it "link - http://example.com" do
+  it "success - http://example.com" do
     expect(link("http://example.com")).to eql("<a href=\"http://example.com\">http://example.com</a>")
   end
-  it "link - example.com" do
+
+  it "fail - example.com" do
     expect(link("example.com")).to eql("example.com")
+  end
+
+  it "fail - http:example.com" do
+    expect(link("http:example.com")).to eql("http:example.com")
   end
 end
