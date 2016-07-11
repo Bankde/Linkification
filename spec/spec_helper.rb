033c0e2923aa8c3fc23b175e4d5cc157a15e0d79
Bankde <bankde@hotmail.com>
2016-07-11 17:31:12 +0700

    change to example.com (to match with article)

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,7 +3,7 @@ require_relative '../makeLink.rb'
 
 
 describe "Test Linkification" do
-  it "link - http://google.com" do
-    expect(link("http://google.com")).to eql("<a href=\"http://google.com\">http://google.com</a>")
+  it "link - example.com" do
+    expect(link("http://example.com")).to eql("<a href=\"http://example.com\">http://example.com</a>")
   end
 end
