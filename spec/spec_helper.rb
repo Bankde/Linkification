d418bb97d7926152d8fe54ffe869239272b5bd05
Bankde <bankde@hotmail.com>
2016-07-11 17:32:47 +0700

    add rule example.com - wrong

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,7 +3,10 @@ require_relative '../makeLink.rb'
 
 
 describe "Test Linkification" do
-  it "link - example.com" do
+  it "link - http://example.com" do
     expect(link("http://example.com")).to eql("<a href=\"http://example.com\">http://example.com</a>")
+  end
+  it "link - example.com" do
+    expect(link("example.com")).to eql("example.com")
   end
 end
