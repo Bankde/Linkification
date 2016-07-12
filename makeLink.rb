b98a9c009de1958029105870ded229471d897a1b
Bankde <bankde@hotmail.com>
2016-07-12 13:54:45 +0700

    refractoring

--- makeLink.rb
+++ makeLink.rb
@@ -1,5 +1,7 @@
 def link(text)
-  if (text[/(http:\/\/|ftp:\/\/|mailto:([\p{Letter}\p{Number}]+@))([\p{Letter}\p{Number}-]+)(\.)(\p{Letter}{2,})/])
+  if (text[/(http:\/\/|ftp:\/\/|https:\/\/|ssh:\/\/|file:\/\/)([\p{Letter}\p{Number}-]+)(\.)(\p{Letter}{2,})/])
+    return "<a href=\"#{text}\">#{text}</a>"
+  elsif (text[/(mailto:)([\p{Letter}\p{Number}\-\.\_]+)(@)([\p{Letter}\p{Number}-]+)(\.)(\p{Letter}{2,})/])
     return "<a href=\"#{text}\">#{text}</a>"
   else
     return text
