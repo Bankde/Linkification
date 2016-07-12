b701fca78873994437dfcc4ce11e53f55b4ea16b
Bankde <bankde@hotmail.com>
2016-07-12 09:47:29 +0700

    dev rule success ftp://example.com

--- makeLink.rb
+++ makeLink.rb
@@ -1,5 +1,5 @@
 def link(text)
-  if (text[/(http:\/\/)(\p{Letter}+)(\.)(\p{Letter}{2,})/])
+  if (text[/([[http:]|[ftp:]]\/\/)(\p{Letter}+)(\.)(\p{Letter}{2,})/])
     return "<a href=\"#{text}\">#{text}</a>"
   else
     return text
