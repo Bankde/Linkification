3985b531001bad826bdb4fa18a6c6c361bc881f7
Bankde <bankde@hotmail.com>
2016-07-12 09:53:33 +0700

    dev rule success - mailto:test@example.com

--- makeLink.rb
+++ makeLink.rb
@@ -1,6 +1,8 @@
 def link(text)
   if (text[/([[http:]|[ftp:]]\/\/)(\p{Letter}+)(\.)(\p{Letter}{2,})/])
     return "<a href=\"#{text}\">#{text}</a>"
+  elsif (text[/([mailto:])(\p{letter}+)(@)(\p{Letter}+)(\.)(\p{Letter}{2,})/])
+    return "<a href=\"#{text}\">#{text}</a>"
   else
     return text
   end
