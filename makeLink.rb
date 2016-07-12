6ee49a00a9aa754829a9c3e61e1c3697b91b0e01
Bankde <bankde@hotmail.com>
2016-07-12 14:18:00 +0700

    refractoring

--- makeLink.rb
+++ makeLink.rb
@@ -1,7 +1,7 @@
 def link(text)
-  if (text[/^(http:\/\/|ftp:\/\/|https:\/\/|ssh:\/\/|file:\/\/)([\p{Letter}\p{Number}-]+)((\.)(\p{Letter}{2,})){1,2}$/])
+  if (text[/(http:\/\/|ftp:\/\/|https:\/\/|ssh:\/\/|file:\/\/)([\p{Letter}\p{Number}-]+)((\.)(\p{Letter}{2,})){1,2}/])
     return "<a href=\"#{text}\">#{text}</a>"
-  elsif (text[/^(mailto:)([\p{Letter}\p{Number}\-\.\_]+)(@)([\p{Letter}\p{Number}-]+)((\.)(\p{Letter}{2,})){1,2}$/])
+  elsif (text[/(mailto:)([\p{Letter}\p{Number}\-\.\_]+)(@)([\p{Letter}\p{Number}-]+)((\.)(\p{Letter}{2,})){1,2}/])
     return "<a href=\"#{text}\">#{text}</a>"
   else
     return text
