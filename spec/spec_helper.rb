ca11340b77e0a7711ce0be48f02a11d318680b6d
Bankde <bankde@hotmail.com>
2016-07-12 09:46:16 +0700

    add rule success - ftp://example.com

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -6,25 +6,22 @@ describe "Test Linkification" do
   it "success - http://example.com" do
     expect(link("http://example.com")).to eql("<a href=\"http://example.com\">http://example.com</a>")
   end
-
   it "fail - example.com" do
     expect(link("example.com")).to eql("example.com")
   end
-
   it "fail - http:example.com" do
     expect(link("http:example.com")).to eql("http:example.com")
   end
-
   it "fail - http://example.a" do
     expect(link("http://example.a")).to eql("http://example.a")
   end
-
   it "fail - http://example..ab" do
     expect(link("http://example..ab")).to eql("http://example..ab")
   end
-
-  it "fail - http://ไทยเมล.คอม" do
+  it "success - http://ไทยเมล.คอม" do
     expect(link("http://ไทยเมล.คอม")).to eql("<a href=\"http://ไทยเมล.คอม\">http://ไทยเมล.คอม</a>")
   end
-
+  it "success - ftp://example.com" do
+    expect(link("ftp://example.com")).to eql("<a href=\"ftp://example.com\">ftp://example.com</a>")
+  end
 end
