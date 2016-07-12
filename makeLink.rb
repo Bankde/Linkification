c112dc897acc17fa79053cc8e6abd1d637d9d0eb
Bankde <bankde@hotmail.com>
2016-07-12 15:36:25 +0700

    dev rule success - www.google.com

--- makeLink.rb
+++ makeLink.rb
@@ -7,6 +7,10 @@ def link(text)
     if (text[/(.*)(#{$1})(.*)/])
       return "#{$1}<a href=\"#{$2}\">#{$2}</a>#{$3}"
     end
+  elsif (text[/((www\.)([\p{Letter}\p{Number}-]+)((\.)(\p{Letter}{2,})){1,2})/])
+    if (text[/(.*)(#{$1})(.*)/])
+      return "#{$1}<a href=\"http://#{$2}\">#{$2}</a>#{$3}"
+    end
   else
     return text
   end
