18707a9fc72651e5337b8f080e23005ab130a705
Bankde <bankde@hotmail.com>
2016-07-12 13:48:40 +0700

    dev rule success - http://exam-p1e.com

--- makeLink.rb
+++ makeLink.rb
@@ -1,5 +1,5 @@
 def link(text)
-  if (text[/(http:\/\/|ftp:\/\/|mailto:(\p{Letter}+@))(\p{Letter}+)(\.)(\p{Letter}{2,})/])
+  if (text[/(http:\/\/|ftp:\/\/|mailto:([\p{Letter}\p{Number}]+@))([\p{Letter}\p{Number}-]+)(\.)(\p{Letter}{2,})/])
     return "<a href=\"#{text}\">#{text}</a>"
   else
     return text
